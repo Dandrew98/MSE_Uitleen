f653fc8c91220d7b0ce580d47b45d10c3e668260
Darrell <43170355+Dandrew98@users.noreply.github.com>
2019-05-21 09:35:21 +0200

    eindelijk de juiste uitlees

--- php/test.php
+++ php/test.php
@@ -5,10 +5,13 @@ require "config.php";
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-
-$query = "SELECT p.naam, pd.type, pd.omschrijving, pd.reeks, pd.artikel_nr, pd.defect, u.datum_uitleen, u.datum_retour, u.opmerkingen FROM tbl_product p
+//wat is er uitgeleend en wie
+/**$query = "SELECT k.klant_nr, p.naam, pd.type, pd.omschrijving, pd.reeks, pd.artikel_nr, pd.defect, u.datum_uitleen, u.datum_retour, u.opmerkingen FROM tbl_klant k, tbl_product p
 JOIN tbl_product_detail pd on p.id = pd.product_id
-join tbl_uitlenen u on pd.id = u.id_product_detail";
+join tbl_uitlenen u on pd.id = u.id_product_detail where k.klant_nr = u.id_klant";**/
+//alle producten laten zien die niet in uitleen staan
+$query = "SELECT p.naam, pd.type, pd.omschrijving, pd.reeks, pd.artikel_nr, pd.defect FROM tbl_uitlenen u, tbl_product p
+JOIN tbl_product_detail pd on p.id = pd.product_id where pd.id != u.id_product_detail";
 
 $uitlees = mysqli_query($mysqli, $query);
 
@@ -26,9 +29,6 @@ if (mysqli_num_rows($uitlees) > 0)
 	echo "<th>Omschrijving</th>";
 	echo "<th>Reeks</th>";
 	echo "<th>Artikel nummer</th>";
-	echo "<th>Uitleen</th>";
-	echo "<th>Retour</th>";
-	echo "<th>Opmerkingen</th>";
 	echo "<th>Defect</th>";
 
 	// sluit de tabelrij voor de kopjes
@@ -51,9 +51,6 @@ if (mysqli_num_rows($uitlees) > 0)
 			echo "<td>" . $row['omschrijving'] . "</td>";
 			echo "<td>" . $row['reeks'] . "</td>";
 			echo "<td>" . $row['artikel_nr'] . "</td>";
-			echo "<td>" . $row['datum_uitleen'] . "</td>";
-			echo "<td>" . $row['datum_retour'] . "</td>";
-			echo "<td>" . $row['opmerkingen'] . "</td>";
 			echo "<td>" . $defect . "</td>";
 
 		}
@@ -67,7 +64,6 @@ if (mysqli_num_rows($uitlees) > 0)
 			echo "<td>" . $row['artikel_nr'] . "</td>";
 			echo "<td>&nbsp;</td>";
 			echo "<td>&nbsp;</td>";
-			echo "<td>" . $row['opmerkingen'] . "</td>";
 			$defect = "Ja";
 			echo "<td>" . $defect . "</td>";
 		}
